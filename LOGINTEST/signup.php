0eaf12501266727f3d7f60055cd4841f6dcedebd
Alemax12 <117alemax@gmail.com>
2021-11-11 21:32:43 -0500

    Login y signup conceptuales

--- LOGINTEST/signup.php
+++ LOGINTEST/signup.php
@@ -1,11 +1,10 @@
 <?php
 
   require 'database.php';
-
   $message = '';
-
+echo $_POST['email'];
   if (!empty($_POST['email']) && !empty($_POST['password'])) {
-    $sql = "INSERT INTO users (email, password) VALUES (:email, :password)";
+    $sql = "INSERT INTO cliente (email, contraseña,id_rol) VALUES (:email, :password,1)";
     $stmt = $conn->prepare($sql);
     $stmt->bindParam(':email', $_POST['email']);
     $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
@@ -13,36 +12,9 @@
 
     if ($stmt->execute()) {
       $message = 'Successfully created new user';
+      header("Location: ../");
     } else {
       $message = 'Sorry there must have been an issue creating your account';
+      header("Location: ../");
     }
   }
-?>
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>SignUp</title>
-    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-    <link rel="stylesheet" href="assets/css/style.css">
-  </head>
-  <body>
-
-    <?php require 'partials/header.php' ?>
-
-    <?php if(!empty($message)): ?>
-      <p> <?= $message ?></p>
-    <?php endif; ?>
-
-    <h1>SignUp</h1>
-    <span>or <a href="login.php">Login</a></span>
-
-    <form action="signup.php" method="POST">
-      <input name="email" type="text" placeholder="Enter your email">
-      <input name="password" type="password" placeholder="Enter your Password">
-      <input name="confirm_password" type="password" placeholder="Confirm Password">
-      <input type="submit" value="Submit">
-    </form>
-
-  </body>
-</html>
