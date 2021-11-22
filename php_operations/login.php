96f5423fadca239cb03972c0309ba3244e981389
Alemax12 <117alemax@gmail.com>
2021-11-22 18:18:26 -0500

    upt

--- php_operations/login.php
+++ php_operations/login.php
@@ -8,13 +8,14 @@
   require 'database.php';
 
   if (!empty($_POST['email']) && !empty($_POST['password'])) {
-    $records = $conn->prepare('SELECT id_cliente, email, contraseña FROM cliente WHERE email = :email');
+    $records = $conn->prepare('SELECT id_cliente, id_rol , email, contraseña FROM cliente WHERE email = :email');
     $records->bindParam(':email', $_POST['email']);
     $records->execute();
     $results = $records->fetch(PDO::FETCH_ASSOC);
 
     if (count($results) > 0 && $_POST['password']==$results['contraseña']) {
       $_SESSION['user_id'] = $results['id_cliente'];
+      $_SESSION['user_rol'] = $results['id_rol'];
       header("Location: ../");
     } else {
       header("Location: ../ViewLogin.php?ress=1");
